80bce03e1d0adc0929dbf7c0c26b02f502ef6769
mitchstartzel <mstartzel16@gmail.com>
2021-02-17 11:32:59 -0500

    defproject name change

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject gov.nasa.earthdata/quartzite "2.2.1-SNAPSHOT"
+(defproject gov.nasa.earthdata/quartzite "2.2.1-RC"
   :description "Quarzite is a thin Clojure layer on top the Quartz Scheduler"
   :min-lein-version "2.5.1"
   :license {:name "Eclipse Public License"}
